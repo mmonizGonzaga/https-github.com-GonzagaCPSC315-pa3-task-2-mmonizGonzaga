b400738d35385e9a3efdf4eb4624375747ca9f15
Matt <kayleemoniz1@makaylas-macbook-pro.local>
2018-09-20 16:50:45 -0700

    Initial Commit

---
+++ PA3 part 2/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  PA3 part 2
+//
+//  Created by Makayla Moniz on 9/20/18.
+//  Copyright © 2018 Makayla Moniz. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
